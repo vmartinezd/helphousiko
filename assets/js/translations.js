1937a3bae44b74186dfb9be1517e7f72db8302e9
bhushanlal <bhushanlal972@gmail.com>
2023-11-29 16:57:31 +0530

    fix styling

---
+++ assets/js/translations.js
@@ -0,0 +1,64 @@
+var blogLang = "en";
+
+content = {
+  "en": {
+    "Who We Are": [
+      { "Company Overview": "lorem ipsum" },
+      { "Leadership Team": "lorem ipsum" },
+      { "Our Team": "lorem ipsum" },
+      { "Achievements and Awards": "lorem ipsum" },
+    ],
+    "General Terms and Conditions": [
+      { "Introduction": "lorem ipsum" },
+      { "User Responsibilities": "lorem ipsum" },
+      { "Service Description": "lorem ipsum" },
+      { "Payment and Billing": "lorem ipsum" },
+      { "Termination": "lorem ipsum" },
+    ],
+    "Privacy Policy": [
+      { "Information Collection": "lorem ipsum" },
+      { "Data Usage": "lorem ipsum" },
+      { "Data Protection": "lorem ipsum" },
+      { "User Rights": "lorem ipsum" },
+      { "Third-Party Disclosure": "lorem ipsum" },
+    ],
+    "Cookie Policy": [
+      { "Types of Cookies": "lorem ipsum" },
+      { "Cookie Usage": "lorem ipsum" },
+      { "Managing Cookies": "lorem ipsum" },
+      { "Analytics and Tracking": "lorem ipsum" },
+      { "Updates to the Cookie Policy": "lorem ipsum" },
+    ],
+  },
+"Hu" : {
+    "Who We Are": [
+        { "Company Overview": "lorem ipsum" },
+        { "Leadership Team": "lorem ipsum" },
+        { "Our Team": "lorem ipsum" },
+        { "Achievements and Awards": "lorem ipsum" },
+      ],
+      "General Terms and Conditions": [
+        { "Introduction": "lorem ipsum" },
+        { "User Responsibilities": "lorem ipsum" },
+        { "Service Description": "lorem ipsum" },
+        { "Payment and Billing": "lorem ipsum" },
+        { "Termination": "lorem ipsum" },
+      ],
+      "Privacy Policy": [
+        { "Information Collection": "lorem ipsum" },
+        { "Data Usage": "lorem ipsum" },
+        { "Data Protection": "lorem ipsum" },
+        { "User Rights": "lorem ipsum" },
+        { "Third-Party Disclosure": "lorem ipsum" },
+      ],
+      "Cookie Policy": [
+        { "Types of Cookies": "lorem ipsum" },
+        { "Cookie Usage": "lorem ipsum" },
+        { "Managing Cookies": "lorem ipsum" },
+        { "Analytics and Tracking": "lorem ipsum" },
+        { "Updates to the Cookie Policy": "lorem ipsum" },
+      ],
+}
+};
+
+
